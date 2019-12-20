70f90fdba193e5003138a4ffb4e0f8101e55e553
Benjamin Morehouse <bmorehou@usc.edu>
2019-12-20 12:59:45 -0800

    Initial Commit

---
+++ exampleone/src/components/GoodbyeWorld.vue
@@ -0,0 +1,23 @@
+<template> 
+	<div class="goodbye">
+		<h1>Goodbye</h1>
+		<p>Here is your messages: {{msg}}</p>
+	</div>
+</template> 
+
+<script>
+export default {
+name: 'GoodbyeWorld',
+	props: {
+		msg: String
+	}
+}
+</script>
+
+<style scoped>
+
+h1{
+	color:grey;
+}
+
+</style>
---
+++ exampleone/src/components/HelloWorld.vue
@@ -0,0 +1,33 @@
+<template>
+	<div class="hello">
+		<p>Hello world this is something that I made on the plane </p>
+		<p> {{msg}} </p>
+	</div>
+</template>
+
+<script>
+export default {
+name: 'HelloWorld',
+	props: {
+		msg: String
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped> 
+h3 {
+	margin: 40px 0 0;
+}
+ul {
+	list-style-type: none;
+	padding: 0;
+}
+li {
+	display: inline-block;
+	margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
