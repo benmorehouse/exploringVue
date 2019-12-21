aef89fad2ab515d7f8ea50aef60fceb79b73f7f5
Benjamin Morehouse <bmorehou@usc.edu>
2019-12-20 18:34:40 -0800

    Updates

--- tip_calculator/src/components/Calculator.vue
+++ tip_calculator/src/components/Calculator.vue
@@ -1,35 +1,49 @@
 <template>
 	<div class="calculator">
 		<h1>Tip Calculator</h1>
+		<Display v-bind:expression='display'  />
 		<div class="button-row">		
-			<Button number="7" />
-			<Button number="8" />
-			<Button number="9" />
+			<button class="button" @click="handleNumber(7)">7</Button>
+			<button class="button" @click="handleNumber(8)">8</Button>
+			<button class="button" @click="handleNumber(9)">9</Button>
 		</div>
 		<div class="button-row">		
-			<Button number="4" />
-			<Button number="5" />
-			<Button number="6" />
+			<button class="button" @click="handleNumber(4)">4</Button>
+			<button class="button" @click="handleNumber(5)">5</Button>
+			<button class="button" @click="handleNumber(6)">6</Button>
 		</div>
 		<div class="button-row">		
-			<Button number="3" />
-			<Button number="2" />
-			<Button number="1" />
+			<button class="button" @click="handleNumber(1)">1</Button>
+			<button class="button" @click="handleNumber(2)">2</Button>
+			<button class="button" @click="handleNumber(3)">3</Button>
 		</div>
 		<div class="button-row">		
-			<Button number="0" />
-			<Button number="." />
+			<button class="button" @click="handleNumber(0)">0</Button>
+			<button class="button" @click="handleNumber('.')">.</Button>
 		</div>
 	</div>
 </template>
 
 <script>
-import Button from './Button.vue'
+import Display from './Display.vue'
 
 export default {
 	name: 'Calculator',
+
 	components:{ 
-		Button,
+		Display,
+	},
+
+	data(){
+		return{
+			expression: '',
+		}
+	},
+
+	methods:{
+		handleNumber: function(value){
+			this.expression = this.expression + value		
+		},
 	}
 }
 </script>
@@ -51,72 +65,25 @@ h1{
 	1px 1px 0 #000;
 }
 
+.button{
+	float:right;
+	background-color:lightgrey;
+	height:50px;
+	width:100px;
+	text-align:center;
+	color:white;
+	font-size:20px;
+	outline: none !important;
+	outline-offset: none !important;
+
+}
+
 .button-row{
 	background-color:grey;
 	width:500px;
-	height:200px;
+	height:50px;
 }
 
 </style>
 
 
-<!-- Example component
-<template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
-</template>
-
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
-
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
-
--->
---
+++ tip_calculator/src/components/Display.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="display">
+		<div class="expression-wrapper">
+			{{expression}}
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Display',
+	props:['expression'],
+}
+</script>
+
+<style scoped>
+
+.display{
+	color:white;
+	background-color:#393A47;
+	text-align:right;
+	height:100px;
+}
+
+.expression-wrapper{
+	font-size:50px;
+	padding-top:25px;
+	padding-bottom:25px;
+}
+
+</style>
+
+
+
